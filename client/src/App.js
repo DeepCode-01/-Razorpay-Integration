b94c2c521633f1696aec08c8b306156254520f69
Deepali Verma <deepaliverma064@gmail.com>
2025-02-04 15:38:57 +0530

    using chakra ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from "./Home";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+ <Router>
+  <Routes>
+    <Route path="/" element={<Home/>} />
+  </Routes>
+ </Router>
   );
 }
 
