a4030b5bfcab7e90c0201a2e65891d902811ca00
Deepali Verma <deepaliverma064@gmail.com>
2025-02-05 14:15:59 +0530

    adding objects

--- client/src/Card.js
+++ client/src/Card.js
@@ -4,8 +4,8 @@ import React from 'react'
 const Card = ({amount, img , CheckOutHandler}) => {
   return (
   <VStack>
-    <Image src={img}/>
-    <Text>{amount}</Text>
+    <Image src={img} boxSize={"65"} objectFit='cover'/>
+    <Text>₹{amount}</Text>
     <Button onClick={CheckOutHandler}> Buy Now</Button>
   </VStack>
   )
--- client/src/Home.js
+++ client/src/Home.js
@@ -12,6 +12,7 @@ const CheckOutHandler =()=>{
     <Box>
       <Stack direction={'row'}>
     <Card amount={5000} img={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTygLPnx8zGkTVYZnFHJAAmMSUZMj0gJqt6UQ&s"} CheckOutHandler={CheckOutHandler}/>
+    <Card amount={3000} img={"https://stylum.in/cdn/shop/files/JOHNYRED_1_56ec4c6c-e072-4901-b3e4-9cf8ae3fe94c.jpg?v=1718200197&width=533"} CheckOutHandler={CheckOutHandler}/>
       </Stack>
     </Box>
   )
